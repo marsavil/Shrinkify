8a617c83c7d42d2919359d3c8e6c9eb4cb0bed83
mariano <msvillaescusa@gmail.com>
2024-03-18 18:54:09 -0300

    autenticacion en create link y en user home

--- app/[id]/(root)/home/page.tsx
+++ app/[id]/(root)/home/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 import Link_Shortener from "@/components/shared/linkShortener/Link_Shortener";
 import styles from './home.module.css';
+import { useAuth } from "@clerk/nextjs";
 
 
 export default  function Home() {
+  const { userId } = useAuth(); 
 
   return (
     <div className={styles.main_container}>
-      <Link_Shortener userId="" />
+      <Link_Shortener userId={userId} />
     </div>
   )
 }
--- app/[id]/(root)/links/create/page.tsx
+++ app/[id]/(root)/links/create/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 import Link_Shortener from "@/components/shared/linkShortener/Link_Shortener";
 import styles from './createLink.module.css';
+import { useAuth } from "@clerk/nextjs";
 
 
 export default  function Home() {
+  const { userId } = useAuth(); 
 
   return (
     <div className={styles.main_container}>
-      <Link_Shortener userId="" />
+      <Link_Shortener userId={userId} />
     </div>
   )
 }
