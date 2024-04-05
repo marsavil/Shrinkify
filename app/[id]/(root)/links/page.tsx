0a448da4de3c950134d3c7f7a634cd756112c104
marsavil <msvillaescusa@gmail.com>
2024-04-05 16:21:13 -0300

    renderizacion alternativa de la pagina links en caso de no tener links creados

--- app/[id]/(root)/links/page.tsx
+++ app/[id]/(root)/links/page.tsx
@@ -1,17 +1,29 @@
-'use server'
-import { currentUser } from '@clerk/nextjs';
+'use client'
+import { currentUser, useAuth } from '@clerk/nextjs';
 import styles from './links.module.css';
 import { fetchUserLinks } from '@/lib/actions/link.actions';
 import LinkCard from '@/components/cards/linkCard/LinkCard';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import Link_Shortener from '@/components/shared/linkShortener/Link_Shortener';
+import { useEffect, useState } from 'react';
 
 
-export default  async function Page() {
-  const user = await currentUser();
-  if ( !user ) redirect('/')
-  const { id } = user;
-  const links = await fetchUserLinks(id);
+export default  function Page() {
+  const { userId } = useAuth();
+  const router = useRouter();
+  const [links, setLinks] = useState([]);
+  if ( !userId ) router.push('/')
 
+  useEffect(() => {
+    const fetchLinksData = async () => {
+      const data = await fetchUserLinks(userId);
+      setLinks(data)
+    }
+    fetchLinksData();
+  }, [userId])
+
+
+  if ( links.length !== 0 ) {
     return (
       <div className={styles.main_container}>
         { links.map ((link:any, index:any) => (
@@ -19,5 +31,14 @@ export default  async function Page() {
         ))}
       </div>
     )
+  } else {
+    return (
+      <div className={styles.notice_container}>
+        <h1 className={styles.notice}>You have not created any link so far!</h1>
+        <Link_Shortener userId={userId}/>
+      </div>
+    )
+  }
+
 
 }
